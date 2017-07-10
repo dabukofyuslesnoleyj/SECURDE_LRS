018f8abba2631543aed494423d282c36ae4c5709
jople <jose_jaena_ople@dlsu.edu.ph>
2017-07-10 17:21:41 +0800

    First content

---
+++ securde-lrs/src/main/java/com/dlsu/lrs/SecurdeLrsApplication.java
@@ -0,0 +1,12 @@
+package com.dlsu.lrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurdeLrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurdeLrsApplication.class, args);
+	}
+}
