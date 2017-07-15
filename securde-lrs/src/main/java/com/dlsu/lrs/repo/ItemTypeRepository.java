8f1d907dcaa9467ef7328c047c05d15bc7550708
jople <jose_jaena_ople@dlsu.edu.ph>
2017-07-15 14:08:41 +0800

    Removed repos for non-Entity classes

--- securde-lrs/src/main/java/com/dlsu/lrs/repo/ItemTypeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dlsu.lrs.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.dlsu.lrs.models.ItemType;
-
-public interface ItemTypeRepository extends CrudRepository<ItemType, String> {
-}
