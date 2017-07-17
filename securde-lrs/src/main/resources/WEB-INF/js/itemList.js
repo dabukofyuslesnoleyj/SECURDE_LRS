0f9067fc25d804a912294d9e81320f2bc0908624
dabukofyuslesnoleyj <chinomanga@outlook.com>
2017-07-18 05:30:59 +0800

    borrow item, review and rate item js done untested

--- securde-lrs/src/main/resources/WEB-INF/js/itemList.js
+++ securde-lrs/src/main/resources/WEB-INF/js/itemList.js
@@ -3,7 +3,52 @@ function initList(){
     $.get("demo_test.asp", 
     function(success, status, message, data, errorData){
         if(success){
-            
+
+            var authors;
+            var tags;
+
+            $.each(
+                data.tags,
+                function (i, val){
+                    tags.append("<li class='list-inline-item'><span class='label label-default'> "+val+" </span></li>");
+                }
+            );
+
+             $.each(
+                        data.authors,
+                        function (i, val){
+                            authors.append("<span> "+val.l+", "+val.f+" </span>");
+                        }
+                    );
+
+            $("#itemList_row").append(
+                '<div class="item_image col-md-1">'+
+                '    <img src="rsc/img/bookCover.jpg" class="img-responsive item_cover"/>'+
+                '</div>'+
+                '<div class="item_info col-md-11">'+
+                '    <div class="row item_info_upper_row">'+
+                '        <div class="item_info_name_div col-md-4">'+
+                '    <p class="item_info_name">'+data.name+'</p>'+
+                '        </div>'+
+                '        <div class="item_info_author_div col-md-4">'+
+                '            <p>by</p><p class="item_info_author">'+authors+'</p>'+
+                '        </div>'+
+                '        <div class="item_info_rating_div col-md-4">'+
+                '            <p>'+data.rating+'</p><p>/10</p>'+
+                '        </div>'+
+                '    </div>'+
+                '    <div class="row item_info_lower_row">'+
+                '            <div class="item_info_type_div col-md-3">'+
+                '                <p class="item_info_type">'+data.type+'</p>'+
+                '            </div>'+
+                '            <div class="item_info_tags_div col-md-9">'+
+                '                <ul class="list-inline">'+tags+
+                '                </ul>'+
+                '            </div>'+
+                '        </div>'+
+                '    </div>'+
+                '</div>'
+            )
         }
     });
 
--- securde-lrs/src/main/resources/WEB-INF/js/item_page.js
+++ securde-lrs/src/main/resources/WEB-INF/js/item_page.js
@@ -14,7 +14,7 @@ function item_info(){
                     $.each(
                         authors,
                         function (i, val){
-                            $("#item_authors").append("<p> "+val.l+", "+val.f+" </p>");
+                            $("#item_authors").append("<span> "+val.l+", "+val.f+" </span>");
                         }
                     );
                     $("#item_publisher").val(publisher);
@@ -24,6 +24,93 @@ function item_info(){
                             $("#item_tags").append("<li class='list-inline-item'><span class='label label-default'> "+val+" </span></li>");
                         }
                     );
+
+                    var review_list = $("review-list");
+
+                    $.each(
+                        review_list,
+                        function (i, val){
+                            review_list.append(
+                                'div class="media">'+
+                                '    <div class="media-left">'+
+                                '        <img alt="prof-pic" src="#" class="media-object">'+
+                                '    </div>'+
+                                '    <div class="media-body">'+
+                                '        <h4 class="media-heading">'+data.review_list.username+'</h4>'+
+                                '        <p>'+data.review_list.review+'</p>'+
+                                '    </div>'+
+                                '    <div class="media-right">'+
+                                '        <h4>Rating</h4>'+
+                                '        <p>'+data.review_list.rating+'</p>'+
+                                '    </div>'+
+                                '</div>'
+                            );
+                        }
+                    );
+                }
+        })
+        .done(function(){
+            
+        })
+        .fail(function(){
+            
+        });
+}
+
+function reviewItem(){
+
+    var review_text = $("#review_text");
+
+    if(review_text.val() != ""){
+        $.post("rest/account/register",
+            {
+                itemId : $(),
+                review : review_text
+            },
+            function(success, status, message, data, errorData){
+                if(success){
+                    
+                }
+        })
+        .done(function(){
+            
+        })
+        .fail(function(){
+            
+        });
+    }
+
+    
+}
+
+function rateItem(){
+    $.post("rest/account/register",
+            {
+                itemId : $(),
+                rating : $("#review_text")
+            },
+            function(success, status, message, data, errorData){
+                if(success){
+                    
+                }
+        })
+        .done(function(){
+            
+        })
+        .fail(function(){
+            
+        });
+}
+
+function borrowItem(){
+    $.post("rest/account/register",
+            {
+                itemId : $()
+            },
+            function(success, status, message, data, errorData){
+                if(success){
+                    $("#item_available").val("Available "+data.available);
+                    $("#item_reserved").val("Reserved "+data.reserved);
                 }
         })
         .done(function(){
